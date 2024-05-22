e2e421aa3556a8341acc0dd2f23297af9324748e
killflex <ferryhasanbackup@gmail.com>
2024-05-22 19:06:26 +0700

    fix: del all

--- dist/js/main.js
+++
@@ -1,5 +0,0 @@
-const navigationHeight = document.querySelector('.navbar').offsetHeight;
-
-// console.log(navigationHeight);
-
-document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 14 +"px")
